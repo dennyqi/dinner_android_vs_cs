d08198d571881c4d26791485ac49ddb55066f99a
Denny Qi <dennyqi@yahoo.com>
2018-04-08 12:55:36 -0700

    initial commit

---
+++ dinner_android_vs_cs/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace dinner_android_vs_cs
+{
+    [Activity(Label = "dinner_android_vs_cs", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
